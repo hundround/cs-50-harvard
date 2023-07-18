2b5ee00d4200ecded1d42d464756459d809d3628
hundround <dorapa@up.edu.ph>
2023-07-18 15:06:38 +0800

    Added cash.c

---
+++ cash/cash.c
@@ -0,0 +1,104 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_cents();
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
+
+int main(void)
+{
+    // Ask how many cents the customer is owed
+    int cents = get_cents();
+
+
+    // Calculate the number of quarters to give the customer
+    int quarters = calculate_quarters(cents);
+    cents = cents - quarters * 25;
+
+    // Calculate the number of dimes to give the customer
+    int dimes = calculate_dimes(cents);
+    cents = cents - dimes * 10;
+
+    // Calculate the number of nickels to give the customer
+    int nickels = calculate_nickels(cents);
+    cents = cents - nickels * 5;
+
+    // Calculate the number of pennies to give the customer
+    int pennies = calculate_pennies(cents);
+    cents = cents - pennies * 1;
+
+    // Sum coins
+    int coins = quarters + dimes + nickels + pennies;
+
+    // Print total number of coins to give the customer
+    printf("%d\n", coins);
+}
+
+int get_cents(void)
+{
+    int i;
+    do
+    {
+        i = get_int("Change owed: ");
+    }
+    while (i < 0);
+    return i;
+}
+
+int calculate_quarters(int cents)
+{
+    // TODO: Compute for the highest number of quarters
+    if (cents >= 25)
+    {
+        int quarters = cents / 25;
+        return quarters;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int calculate_dimes(int cents)
+{
+    // TODO: Compute for the highest number of dimes
+    if (cents >= 10)
+    {
+        int dimes = cents / 10;
+        return dimes;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int calculate_nickels(int cents)
+{
+    // TODO: Compute for the highest number of nickels
+    if (cents >= 5)
+    {
+        int nickels = cents / 5;
+        return nickels;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int calculate_pennies(int cents)
+{
+    // TODO: Compute for the highest number of pennies
+    if (cents >= 1)
+    {
+        int pennies = cents / 1;
+        return pennies;
+    }
+    else
+    {
+        return 0;
+    }
+}
